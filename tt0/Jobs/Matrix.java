6df5f4199710e3f206bc28a6e6d6d7a5ac1b9428
alvinzhengq <vortexdomainia@gmail.com>
2022-03-14 01:00:33 -0700

    completed challenge 3

---
+++ tt0/Jobs/Matrix.java
@@ -0,0 +1,46 @@
+package tt0.Jobs;
+
+import java.util.HashMap;
+
+public class Matrix extends Job {
+    private int[][] matrix;
+    private static final String NAME = "Matrix";
+
+    public String getName() {
+        return NAME;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == -1) {
+                    sb.append("  ");
+                } else {
+                    sb.append(matrix[i][j] + " ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    static int[][] keypad() {
+        return new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { -1, 0, -1 } };
+    }
+
+    static int[][] numbers() {
+        return new int[][] { { 0, 1 },
+                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
+    }
+
+    public void run(HashMap<Integer, Job> prompts) {
+        this.matrix = keypad();
+        System.out.println("Keypad:");
+        System.out.println(this);
+    }
+
+}
